f5636444a28ba5db5ad0ab213d446478c2af7212
Nabil-Kaindra <nabilkaindrabudiantana@gmail.com>
2024-10-17 08:56:11 +0700

    first commit

--- app/src/main/java/com/example/splashscreen/SplashActivity.kt
+++ app/src/main/java/com/example/splashscreen/SplashActivity.kt
@@ -4,31 +4,34 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import androidx.activity.enableEdgeToEdge
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class SplashActivity : AppCompatActivity() {
+    private val SPLASH_SCREEN_TIME: Long = 4000 // 4 detik
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
+        // Menghilangkan status bar untuk fullscreen mode
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        // Menampilkan layout splash screen
+        setContentView(R.layout.activity_splash)
+
+        // Handler untuk delay splash screen selama SPLASH_SCREEN_TIME lalu pindah ke MainActivity
         Handler(Looper.getMainLooper()).postDelayed({
             goToMainActivity()
-        }, 8000L)
+        }, SPLASH_SCREEN_TIME)
     }
 
-    private fun goToMainActivity(){
-        Intent(this, MainActivity::class.java).also {
-            startActivity(it)
-            finish()
-        }
+    private fun goToMainActivity() {
+        // Intent untuk membuka MainActivity
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish() // Menutup SplashActivity agar tidak bisa kembali dengan tombol back
     }
 }
